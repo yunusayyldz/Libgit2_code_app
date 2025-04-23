9c85af90423449291f6125f1a72d8f3b289ea65d
yunusayyldz <ayyildizy824@gmail.com>
2025-04-23 12:32:40 +0300

    proje 2

---
+++ proje02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int yas;
+    printf("yaşınız kaç");
+    scanf("%d",&yas);
+    printf("yaşınız %d",yas);
+    if(yas == 18)
+    {
+        printf("reşitsiniz");
+    }
+}
